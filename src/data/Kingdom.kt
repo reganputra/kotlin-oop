132a6f381c61085c8b282efee860f618b4916cce
reganputra <ramadhanregan334@gmail.com>
2024-09-10 08:35:19 +0700

    function override

--- src/data/Kingdom.kt
+++ src/data/Kingdom.kt
@@ -4,12 +4,26 @@ package data
 // This is Parent class
 open class Kingdom (var name: String) {
 
-    fun greeting(name: String){
+//    use open to allow function overriding
+//    is used to enable child class to create a parent class function
+    open fun greeting(name: String){
         println("Greetings ${this.name}, My name is $name")
     }
 
 }
 // This is Child class
 // every Child inherit both properties and function from parent in this case
-class King(name: String): Kingdom(name)
-class Queen(name: String): Kingdom(name)
+open class King(name: String): Kingdom(name) {
+//    ovveride implementation
+//    you can add final to override function so child class cant create parent function
+    override fun greeting(name: String) {
+        println("Hello $name, yes, you're right i am ${this.name}")
+    }
+}
+
+
+class Queen(name: String): Kingdom(name){
+    override fun greeting(name: String) {
+        println("Hello $name, yes, you're right i am ${this.name}")
+    }
+}
