e89b359f0cabd83305fe2084562ed7127709732a
reganputra <ramadhanregan334@gmail.com>
2024-09-10 07:54:22 +0700

    this keyword

---
+++ src/app/ThisKeyword.kt
@@ -0,0 +1,11 @@
+package app
+
+import data.Person
+
+fun main(){
+    val harry = Person()
+    harry.name = "Harry"
+
+    harry.gitGud("Adam", "Edo")
+}
+//this keyword used to access properties with the same name
--- src/data/Person.kt
+++ src/data/Person.kt
@@ -3,6 +3,7 @@ package data
 class Person {
     var name: String = ""
     var age: Int? = null
+    var yourName: String = ""
     var country: String = ""
 
 //    Function in class
@@ -23,5 +24,9 @@ class Person {
     println("Hello $firstName $lastName , My name is $name")
 }
 
+//    this keyword
+    fun gitGud(yourName: String, name: String){
+        println(" Hello $yourName, my name is ${this.name} ")
+    }
 }
 
